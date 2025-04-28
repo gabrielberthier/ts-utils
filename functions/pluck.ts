e247328ad1c040520f55329df0d5f83d64bc9586
Gabriel Berthier <40048106+gabrielberthier@users.noreply.github.com>
2025-04-28 11:03:53 -0300

    Update pluck.ts

--- functions/pluck.ts
+++ functions/pluck.ts
@@ -1,4 +1,1 @@
 export const pluckDeep = (key: string) => (obj: any) => key.split('.').reduce((accum, key) => accum[key], obj)
-
-export const excludeKey = <T extends Record<string, unknown>, K extends keyof T>(obj: T, key: K) =>
-  Object.fromEntries(Object.entries(obj).filter(([i]) => i !== key));
